ec72874a2f27d8faaa5432fc0e09ed349c879ac7
sudzekai <yomakum@mail.ru>
2025-12-06 04:22:12 +0300

    Обработчик ошибок бота

---
+++ Bot/TelegramBot/Tools/BotHandlers/Handlers/Errors/ErrorHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.Design;
+using Telegram.Bot;
+using TelegramBot.Tools.APIServices;
+using TelegramBot.Tools.AppErrorHandler;
+
+namespace TelegramBot.Tools.BotHandlers.Handlers.Errors
+{
+    public class ErrorHandler
+    {
+        private readonly TelegramBotClient _bot;
+        private readonly ApiServicesContainer _services;
+
+        public ErrorHandler(TelegramBotClient bot, ApiServicesContainer services)
+        {
+            _bot = bot;
+            _services = services;
+        }
+
+        public Task OnError(Exception exception, Telegram.Bot.Polling.HandleErrorSource source)
+        {
+            BotLogger.HandleException(exception);
+            return Task.CompletedTask;
+        }
+    }
+}
