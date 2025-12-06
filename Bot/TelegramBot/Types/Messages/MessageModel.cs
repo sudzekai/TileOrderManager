ee6c44ec2609388c6b673cb186e1fd6ff0618d3b
sudzekai <yomakum@mail.ru>
2025-12-06 04:17:12 +0300

    Класс для облегчения конструирования сообщений бота

---
+++ Bot/TelegramBot/Types/Messages/MessageModel.cs
@@ -0,0 +1,19 @@
+﻿using Telegram.Bot.Types.ReplyMarkups;
+
+namespace TelegramBot.Types.Messages
+{
+    public class MessageModel
+    {
+        public string Text { get; set; } = "";
+
+        public InlineKeyboardMarkup Markup { get; set; } = new();
+
+        public MessageModel() { }
+
+        public MessageModel(string text, InlineKeyboardMarkup markup) 
+        {
+            Text = text;
+            Markup = markup;
+        }
+    }
+}
