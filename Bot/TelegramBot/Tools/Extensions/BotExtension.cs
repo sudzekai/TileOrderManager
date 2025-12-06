198ec79c5e4f96709866c208606ce5ddb1c98273
sudzekai <yomakum@mail.ru>
2025-12-06 04:19:01 +0300

    Расширение бота для интеграции с MessageModel

---
+++ Bot/TelegramBot/Tools/Extensions/BotExtension.cs
@@ -0,0 +1,16 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+using TelegramBot.Tools.Extensions;
+using TelegramBot.Types.Messages;
+
+namespace TelegramBot.Tools.Extensions
+{
+    public static class BotExtension
+    {
+        public static async Task<Message> SendMessageModel(this TelegramBotClient bot, long chatIdm, MessageModel message)
+            => await bot.SendMessage(chatIdm, message.Text, Telegram.Bot.Types.Enums.ParseMode.Markdown, replyMarkup: message.Markup);
+
+        public static async Task<Message> EditMessageModel(this TelegramBotClient bot, long chatId, int messageId, MessageModel message)
+            => await bot.EditMessageText(chatId, messageId, message.Text, Telegram.Bot.Types.Enums.ParseMode.Markdown, message.Markup);
+    }
+}
